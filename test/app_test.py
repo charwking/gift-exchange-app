4d9fac9ba0408b1bb67d41b39e3c309c3f0b94c1
charwking <charwking@users.noreply.github.com>
2020-08-26 13:04:04 -0500

    Adds tests
    Adds tests for simple endpoints using pytest, since that's what cherrypy's test helper uses

---
+++ test/app_test.py
@@ -0,0 +1,19 @@
+import cherrypy
+from cherrypy.test import helper
+from src import app
+
+class MainTest(helper.CPWebCase):
+
+   @staticmethod
+   def setup_server():
+      cherrypy.tree.mount(app.GiftExchangeApp())
+
+   def test_index_is_hello_world(self):
+      self.getPage('/')
+      self.assertStatus('200 OK')
+      self.assertBody('Hello world!')
+
+   def test_ping_is_pong(self):
+      self.getPage('/ping')
+      self.assertStatus('200 OK')
+      self.assertBody('pong')
