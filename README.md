96fb377a3c8763c1a3b32b4bc0219f771fcbc04e
Chuck King <cwkingjr@gmail.com>
2020-08-30 17:18:35 -0500

    Adds pre-commit and black

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 ## Development
 
 1. Install [pipenv](https://pipenv.pypa.io/en/latest/)
+1. Create a lock file that allows the black beta release: `pipenv lock --pre`
 1. Install dependencies: `pipenv install`
+1. Install dependencies: `pipenv install --dev`
+1. Install the pre-commit hooks: `pipenv run pre-commit install`
 1. Run tests: `bin/test.sh`
 1. Run app: `bin/local.sh`
 
@@ -20,3 +23,8 @@ Short notes on decisions that were made so they can be discussed or revisted lat
 ### Infrastructure
 
 * There's probably a "more correct" mechanism for managing environment variables, but stuffing them in shell scripts makes them explicit and easy to manage.
+
+### References
+
+https://github.com/psf/black/tree/stable
+https://pre-commit.com/
--- src/app.py
+++ src/app.py
@@ -1,20 +1,23 @@
 import cherrypy
 import os
 
-cherrypy.config.update({
-   'environment':            os.environ.get('ENVIRONMENT'),
-   'server.socket_port': int(os.environ.get('SERVER_PORT', 5050))
-})
+cherrypy.config.update(
+    {
+        "environment": os.environ.get("ENVIRONMENT"),
+        "server.socket_port": int(os.environ.get("SERVER_PORT", 5050)),
+    }
+)
 
-class GiftExchangeApp():
 
-   @cherrypy.expose
-   def index(self):
-      return 'Hello world!'
+class GiftExchangeApp:
+    @cherrypy.expose
+    def index(self):
+        return "Hello world!"
 
-   @cherrypy.expose
-   def ping(self):
-      return 'pong'
+    @cherrypy.expose
+    def ping(self):
+        return "pong"
 
-if __name__ == '__main__':
-   cherrypy.quickstart(GiftExchangeApp())
+
+if __name__ == "__main__":
+    cherrypy.quickstart(GiftExchangeApp())
