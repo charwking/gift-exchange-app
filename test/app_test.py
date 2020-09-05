84fe0453e74674e430d3a8c3d58bee52e7b6d436
charwking <charwking@users.noreply.github.com>
2020-09-05 18:33:32 -0500

    Run black on test dir

--- test/app_test.py
+++ test/app_test.py
@@ -2,18 +2,18 @@ import cherrypy
 from cherrypy.test import helper
 from src import app
 
+
 class MainTest(helper.CPWebCase):
+    @staticmethod
+    def setup_server():
+        cherrypy.tree.mount(app.GiftExchangeApp())
 
-   @staticmethod
-   def setup_server():
-      cherrypy.tree.mount(app.GiftExchangeApp())
+    def test_index_is_hello_world(self):
+        self.getPage("/")
+        self.assertStatus("200 OK")
+        self.assertBody("Hello world!")
 
-   def test_index_is_hello_world(self):
-      self.getPage('/')
-      self.assertStatus('200 OK')
-      self.assertBody('Hello world!')
-
-   def test_ping_is_pong(self):
-      self.getPage('/ping')
-      self.assertStatus('200 OK')
-      self.assertBody('pong')
+    def test_ping_is_pong(self):
+        self.getPage("/ping")
+        self.assertStatus("200 OK")
+        self.assertBody("pong")
